fc904de05311d83fe935b5a5f134c02d8480130b
gslysz <gordon.slysz@gmail.com>
2016-11-17 17:17:19 -0800

    Improved GetListOfActiveTests

--- TfsHelper.Testing/TestExplorerVmTests.cs
+++ TfsHelper.Testing/TestExplorerVmTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Microsoft.TeamFoundation.TestManagement.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TfsHelper.Services;
@@ -22,27 +24,49 @@ namespace TfsHelper.Testing
             var testPlanName = dataService.TfsSettings.TestExplorerSettings.TestPlanName;
             //testPlanName = "Arc Software Full Verification Plan - Release Baseline for v. 2.0";
 
-            var arcTestPlan = plans.FirstOrDefault(p => p.Name==testPlanName);
+            var arcTestPlan = plans.FirstOrDefault(p => p.Name == testPlanName);
             Assert.IsNotNull(arcTestPlan);
 
-            var testPoints = arcTestPlan.QueryTestPoints("Select * from TestPoint");
+            var testPoints = arcTestPlan.QueryTestPoints("Select * from TestPoint where pointState='Ready'");
             int counter = 1;
 
+            var suiteIds = testPoints.Select(p => p.SuiteId).Distinct().ToList();
+            var testCaseIds = testPoints.Select(p => p.TestCaseId).Distinct().ToList();
+
+            var testsuites = vm.TestManagementTeamProject.TestSuites.Query("select * from TestSuite").Where(p => suiteIds.Contains(p.Id)).ToList();
+
+            string fullQuery = string.Format("SELECT [System.Id], [System.Title] FROM WorkItems WHERE [System.WorkItemType] = 'Test Case' AND [Team Project] = '{0}'", vm.TestManagementTeamProject.TeamProjectName);
+            var allTestCases = vm.TestManagementTeamProject.TestCases.Query(fullQuery).Where(p => testCaseIds.Contains(p.Id)).ToList();
+
+
+            StringBuilder sb = new StringBuilder();
             foreach (var testPoint in testPoints.OrderBy(p => p.SuiteId))
             {
-                if (testPoint.State == TestPointState.Ready && !testPoint.IsTestCaseAutomated)
-                {
-                    var suite = vm.TestManagementTeamProject.TestSuites.Find(testPoint.SuiteId);
-                    var testCaseLocal = vm.TestManagementTeamProject.TestCases.Find(testPoint.TestCaseId);
+                var suite = testsuites.First(p => p.Id == testPoint.SuiteId);
+                var testCaseLocal = allTestCases.First(p => p.Id == testPoint.TestCaseId);
 
-                    bool passesFilter = testPoint.ConfigurationName.Contains("direct", StringComparison.OrdinalIgnoreCase);
-                    passesFilter = true;
-                    if (passesFilter)
-                        Console.WriteLine(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}", counter++, suite.Parent.Title, suite.Id, suite.Title, testCaseLocal.Id, testCaseLocal.Title, testPoint.ConfigurationName));
-                }
+                //bool passesFilter = testPoint.ConfigurationName.Contains("Filters", StringComparison.OrdinalIgnoreCase);
+                bool passesFilter = true;
+                //passesFilter = suite.Title.Contains("Filters");
+
+
+                var testResults = vm.TestManagementTeamProject.TestResults.ByTestId(testPoint.TestCaseId).Where(p=>p.Outcome==TestOutcome.Passed);
+                var testResultTimes=  testResults.Select(p => p.Duration.TotalMinutes).ToList();
+                var numResults = testResultTimes.Count;
+
+                var medianResultTimeInMinutes=   MathNet.Numerics.Statistics.Statistics.Median(testResultTimes);
+
+
+                if (passesFilter)
+                    sb.AppendLine(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8:N2}", counter++, suite.Parent.Title, suite.Id, suite.Title, testCaseLocal.Id, testCaseLocal.Title, testPoint.ConfigurationName,numResults,  medianResultTimeInMinutes));
             }
 
+            Debug.WriteLine(sb.ToString());
+
         }
+
+
+
 
 
         [Ignore]
